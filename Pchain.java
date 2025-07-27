66e0c2ece9df5ab6fcd20c74e52e00ed2e15d66f
Pranesh Nikhar <praneshnikhar@gmail.com>
2025-07-27 12:11:37 +0530

    Update Pchain.java

--- Pchain.java
+++ Pchain.java
@@ -7,7 +7,7 @@ public class Pchain {
     public static int difficulty = 5;
 
     public static void main(String[] args) {
-        //add our blocks to the blockchain ArrayList:
+        
 
         blockchain.add(new Block("Hi im the first block", "0"));
         System.out.println("Trying to Mine block 1... ");
@@ -33,21 +33,21 @@ public class Pchain {
         Block previousBlock;
         String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 
-        //loop through blockchain to check hashes:
+        
         for(int i=1; i < blockchain.size(); i++) {
             currentBlock = blockchain.get(i);
             previousBlock = blockchain.get(i-1);
-            //compare registered hash and calculated hash:
+            
             if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
                 System.out.println("Current Hashes not equal");
                 return false;
             }
-            //compare previous hash and registered previous hash
+            
             if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
                 System.out.println("Previous Hashes not equal");
                 return false;
             }
-            //check if hash is solved
+            
             if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
                 System.out.println("This block hasn't been mined");
                 return false;
